c4f52b9cd629d9dd7bcba448baf3bb8ddc023856
PraveenP <ppraveentr@gmail.com>
2017-06-16 23:26:58 +0530

    Sample Application for MobileCore SDK.

---
+++ FTMobileCoreSample/FTMobileCoreSample/Datamodel/SampleModel.swift
@@ -0,0 +1,16 @@
+//
+//  SampleModel.swift
+//  FTMobileCoreSample
+//
+//  Created by Praveen Prabhakar on 15/06/17.
+//  Copyright © 2017 Praveen Prabhakar. All rights reserved.
+//
+
+import UIKit
+import FTMobileCore
+
+open class SampleModel : FTDataModel{
+    
+    
+}
+
---
+++ FTMobileCoreSample/FTMobileCoreSample/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  FTMobileCoreSample
+//
+//  Created by Praveen Prabhakar on 15/06/17.
+//  Copyright © 2017 Praveen Prabhakar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        let bundle = Bundle(identifier: "com.company.bundle.LiveUI")
+//    
+//        var sample = FTModelObject.createDataModelOfType("Sample", fromDictionary: ["sample":"sample"])
+    
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
