7bf45bfbdc340b05ff80b67c50eb4179f4c1a05e
PraveenP <ppraveentr@gmail.com>
2020-04-18 14:28:00 +0530

    Updated Cell and Label extensions.

--- FTCoreUtility/Classes/Extensions/UITableViewCell+Extension.swift
+++ FTCoreUtility/Classes/Extensions/UITableViewCell+Extension.swift
@@ -14,7 +14,38 @@ protocol ConfigurableCell: AnyObject {
     func setup(viewModel: ViewModel)
 }
 
-extension UITableViewCell {
+public extension UIView {
+    static var defaultNibName: String {
+        return String(describing: self)
+    }
+    
+    static func loadFromDefaultNib<T: UIView>() -> T {
+        return loadFromNib(defaultNibName)
+    }
+    
+    static func loadFromNib<T: UIView>(_ nibName: String) -> T {
+        return loadFromNib(nibName, bundle: Bundle(for: self))
+    }
+    
+    static func loadFromNib<T: UIView>(_ nibName: String, bundle: Bundle) -> T {
+        return loadFromNib(nibName, bundle: bundle, owner: nil)
+    }
+    
+    static func loadFromNib<T: UIView>(_ nibName: String, bundle: Bundle, owner: Any?) -> T {
+        guard let view = bundle.loadNibNamed(nibName, owner: owner, options: nil)?.first as? T else {
+            fatalError("\(self) has not been initialized properly from nib \(nibName)")
+        }
+        return view
+    }
+    
+    func loadViewFromNib(_ nibName: String) -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+}
+
+public extension UITableViewCell {
     // ReuseIdentifier
     static var defaultReuseIdentifier: String {
         "\(self)ID"
@@ -28,6 +59,39 @@ extension UITableViewCell {
     // dequeue cell for the class
     static func dequeue<T: UITableViewCell>(from tableView: UITableView, for indexPath: IndexPath) -> T {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: defaultReuseIdentifier, for: indexPath) as? T else {
+            fatalError("Unable to dequeue cell of type \(T.self) with reuse identifier '\(defaultReuseIdentifier)'")
+        }
+        return cell
+    }
+}
+
+public extension UICollectionViewCell {
+    // ReuseIdentifier
+    static var defaultReuseIdentifier: String {
+        "\(self)ID"
+    }
+    
+    static var defaultNib: UINib {
+        return getNib(nibName: defaultNibName)
+    }
+    
+    static func getNib(nibName: String) -> UINib {
+        return UINib(nibName: nibName, bundle: Bundle(for: self))
+    }
+    
+    // register a class for the tableView
+    static func registerClass(for collectionView: UICollectionView, reuseIdentifier: String? = nil) {
+        collectionView.register(self, forCellWithReuseIdentifier: reuseIdentifier ?? defaultReuseIdentifier)
+    }
+    
+    static func registerNib(for collectionView: UICollectionView, reuseIdentifier: String? = nil) {
+        let nib = UINib(nibName: defaultNibName, bundle: Bundle(for: self))
+        collectionView.register(nib, forCellWithReuseIdentifier: reuseIdentifier ?? defaultReuseIdentifier)
+    }
+    
+    // dequeue cell for the class
+    static func dequeue<T: UICollectionViewCell>(from collectionView: UICollectionView, for indexPath: IndexPath) -> T {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: defaultReuseIdentifier, for: indexPath) as? T else {
             fatalError("Unable to dequeue cell of type \(T.self) with reuse identifier '\(defaultReuseIdentifier)'")
         }
         return cell
